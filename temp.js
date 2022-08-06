bc471657f2e648a8c503e4149ce885a6da0d1ac0
hu_ph <hu_ph@foxmail.com>
2022-08-06 10:10:17 +0800

    add lisence

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new scrip
+// @author       hu
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// @license      AGPL-3.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world')
+})();
